f0fe01885434db0f26f4d709396251016d130cdc
robertferrer93 <robertferrer93@gmail.com>
2026-01-18 19:33:25 +0100

    Update DoctoraliaWidget.jsx

--- src/components/DoctoraliaWidget.jsx
+++ src/components/DoctoraliaWidget.jsx
@@ -1,45 +1,11 @@
-// src/components/DoctoraliaWidget.jsx
 import * as React from 'react';
-import { loadDocplanner } from '../lib/docplanner';
 
 export default function DoctoraliaWidget() {
-  const hostRef = React.useRef(null);
-  const [instanceKey, setInstanceKey] = React.useState(0);
-  const didInitRef = React.useRef(false);
-
   const url = 'https://www.doctoralia.es/clinicas/grupo-cabot';
 
-  React.useEffect(() => {
-    const el = hostRef.current;
-    if (!el) return;
-
-    const init = async () => {
-      // recrea el <a> para que el script lo detecte
-      setInstanceKey((k) => k + 1);
-
-      // primera vez: carga normal; después: reinyección para re-scan
-      await loadDocplanner({ force: didInitRef.current });
-      didInitRef.current = true;
-    };
-
-    const io = new IntersectionObserver(
-      (entries) => {
-        if (entries.some((e) => e.isIntersecting)) {
-          init();
-          io.disconnect();
-        }
-      },
-      { threshold: 0.15 }
-    );
-
-    io.observe(el);
-    return () => io.disconnect();
-  }, []);
-
   return (
-    <div ref={hostRef}>
+    <div>
       <a
-        key={instanceKey}
         href={url}
         rel="nofollow"
         data-zlw-facility="grupo-cabot"
