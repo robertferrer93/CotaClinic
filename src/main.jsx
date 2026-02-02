9936c1a75745768d49658148b6fc078aca0d7809
robertferrer93 <robertferrer93@gmail.com>
2026-02-02 17:51:43 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 // src/main.jsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import CotaClinicSite from "./CotaClinicSitev2.jsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import CotaClinicSite from './CotaClinicSite_alt.jsx';
+import AnalyticsTracker from './AnalyticsTracker.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <AnalyticsTracker />
       <CotaClinicSite />
     </BrowserRouter>
   </React.StrictMode>
