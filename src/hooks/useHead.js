664c94c71083f5f8a31148e540b02ff8707fe558
robertferrer93 <robertferrer93@gmail.com>
2026-02-23 12:37:54 +0100

    Create useHead.js

---
+++ src/hooks/useHead.js
@@ -0,0 +1,18 @@
+// src/hooks/useHead.js
+import { useEffect } from 'react';
+
+export function useHead({ title, description }) {
+  useEffect(() => {
+    if (title) document.title = title;
+
+    if (description) {
+      let meta = document.querySelector('meta[name="description"]');
+      if (!meta) {
+        meta = document.createElement('meta');
+        meta.setAttribute('name', 'description');
+        document.head.appendChild(meta);
+      }
+      meta.setAttribute('content', description);
+    }
+  }, [title, description]);
+}
